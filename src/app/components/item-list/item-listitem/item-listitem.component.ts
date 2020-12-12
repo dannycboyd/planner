40720e3acade2830fb1c4953659774495705a12a
Danny Boyd <odannyboyd@montara.com>
2020-12-11 21:27:29 -0800

    added scroll bars to the content

--- src/app/components/item-list/item-listitem/item-listitem.component.ts
+++ src/app/components/item-list/item-listitem/item-listitem.component.ts
@@ -2,9 +2,9 @@ import { Component, Input } from '@angular/core';
 import { PlanItem } from 'src/app/models';
 
 @Component({
-  selector: 'item-li',
+  selector: 'item-li', // change this, it's a flex list now
   templateUrl: './item-listitem.component.html'
 })
 export class ItemListitemComponent {
-  @Input() item: PlanItem;s
+  @Input() item: PlanItem;
 }
